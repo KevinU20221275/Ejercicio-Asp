130c0aaa4d0c78f6eed5c43de66216bccd984bdf
Kevin <U20221275@univo.edu.sv>
2024-01-30 10:29:11 -0600

    Proyecto de Invetario con MVC

---
+++ InventarioAppDemo/InventarioApp/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using InventarioApp.Data;
+using InventarioApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventarioApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            ProductData productData = new ProductData();
+            return View(productData.GetAllProducts());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ProductModel productModel)
+        {
+            try
+            {
+                ProductData productData = new ProductData();
+                productData.Add(productModel);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = ex.Message;
+
+                return View(productModel);
+            }
+        }
+    }
+}
---
+++ InventarioAppDemo/InventarioApp/Models/ProductModel.cs
@@ -0,0 +1,19 @@
+﻿namespace InventarioApp.Models
+{
+    public class ProductModel
+    {
+        public int Id { get; set; }
+
+        public string? ProductName { get; set; }
+
+        public string? ProductDescription { get; set; }
+
+        public string? ProductCategory { get; set; }
+
+        public int? Stock { get; set; }
+
+        public double? Price { get; set;}
+
+        public string? ExpirationDate { get; set; }
+    }
+}
